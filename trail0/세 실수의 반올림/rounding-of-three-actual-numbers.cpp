f14e5b26f762d53ea452e35786399993847ab65b
oun1105 <86242503+oun1105@users.noreply.github.com>
2026-08-11 14:17:01 +0900

    [Passed] 세 실수의 반올림 (1ms, 0MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a; double b; double c;
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+    cout << a << endl;
+    cout << b << endl;
+    cout << c;
+    return 0;
+}
