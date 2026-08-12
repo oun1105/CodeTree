584a736792ad64519d0eecf032adb7317ea51730
oun1105 <86242503+oun1105@users.noreply.github.com>
2026-08-12 22:15:20 +0900

    [Passed] 문자열 출력하기 (2ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a = "LeebrosCode";
+    int b; cin >> b;
+
+    for(int i=0; i<b; i++){
+        cout << a << endl;
+    }
+    return 0;
+}
