8fd38446697670f05e1e99df567662386492bc77
oun1105 <86242503+oun1105@users.noreply.github.com>
2026-08-24 09:39:35 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void Print(int n){
+    for(int i=0; i<n; i++){
+        cout << "12345^&*()_" << endl;
+    }
+    
+}
+
+int main() {
+    cin >> N;
+
+    Print(N);
+
+    return 0;
+}
