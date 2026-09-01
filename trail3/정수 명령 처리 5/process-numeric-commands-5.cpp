2ff13712b73e1c2f0a459dfdf81056da156600b8
oun1105 <86242503+oun1105@users.noreply.github.com>
2026-09-01 16:53:13 +0900

    [Passed] 정수 명령 처리 5 (8ms, 1MB)

---
+++ trail3/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+string command[10000];
+int num[10000];
+
+
+int main() {
+    cin >> N;
+    vector<int> v;
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push_back" || command[i] == "get") {
+            cin >> num[i];
+            if(command[i] == "push_back"){
+                v.push_back(num[i]);
+            }
+            else if(command[i] == "get"){
+                cout << v[num[i]-1] << endl;
+            }
+        }
+        else if(command[i] == "pop_back"){
+            v.pop_back();
+        }
+        else if(command[i] == "size"){
+            cout << v.size() << endl;
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
